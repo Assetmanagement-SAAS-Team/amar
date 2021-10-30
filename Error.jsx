8e2f15f3b6d81023aed935a69c188926e200247e
ashok1999kumar <88923822+ashok1999kumar@users.noreply.github.com>
2021-10-30 16:25:54 +0530

    Update Error.jsx

--- Error.jsx
+++ Error.jsx
@@ -4,7 +4,7 @@ export default function Error() {
     return (
         <>
             <h1>404 : Error</h1>
-            <p> Opps : page not found </p>
+            <p> Opps : page not found ,something error</p>
 
         </>
     )
